5a8631c7733fa86ed9b7ed7d9acd7f9d273e4458
PeHenr1 <phenriqueaissa@gmail.com>
2025-10-06 22:14:25 -0300

    refactor: changing import Optional to List - close #11 #12 #13 #14

--- src/test/java/br/ifsp/demo/service/EnrollmentQueryServiceTest.java
+++ src/test/java/br/ifsp/demo/service/EnrollmentQueryServiceTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
