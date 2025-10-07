afab8c7a5c0a0ca6fb72022786708d81b554cab2
PeHenr1 <phenriqueaissa@gmail.com>
2025-10-06 23:03:00 -0300

    fix: adjusting entities after conflicts

--- src/main/java/br/ifsp/demo/model/Enrollment.java
+++ src/main/java/br/ifsp/demo/model/Enrollment.java
@@ -3,14 +3,11 @@ package br.ifsp.demo.model;
 import jakarta.persistence.*;
 import lombok.Getter;
 import java.time.LocalDate;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 @Entity
 @Table(name = "enrollment")
 @Getter
-@NoArgsConstructor
 public class Enrollment {
 
     @Id
@@ -24,13 +21,12 @@ public class Enrollment {
     public Enrollment() {
         this.cancellationDeadline = LocalDate.now().plusDays(3);
     }
-    @OneToMany(mappedBy = "enrollment", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Course> courses;
-
     public Enrollment(Long id) {
         this.id = id;
         this.cancellationDeadline = LocalDate.now().plusDays(3);
     }
+    @OneToMany(mappedBy = "enrollment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Course> courses;
 
     public void setDeadline(LocalDate date){
         this.cancellationDeadline = date;
