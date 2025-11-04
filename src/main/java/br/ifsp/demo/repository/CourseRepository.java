775c7c822eba05b3b81e87b1d650ccef94972d0c
Vini <vinicius.matos@aluno.ifsp.edu.br>
2025-11-03 23:32:43 -0300

    feat: implement feature to add live filter on user courses screen

--- src/main/java/br/ifsp/demo/repository/CourseRepository.java
+++ src/main/java/br/ifsp/demo/repository/CourseRepository.java
@@ -2,6 +2,8 @@ package br.ifsp.demo.repository;
 
 import br.ifsp.demo.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +12,16 @@ import java.util.Optional;
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
     List<Course> findByEnrollmentId(Long enrollmentId);
+    Optional<Course> findByCode(String code);
+
+    @Query("SELECT c FROM Course c WHERE " +
+            "(:name IS NULL OR lower(c.name) LIKE lower(concat('%', :name, '%'))) AND " +
+            "(:shift IS NULL OR lower(c.shift) LIKE lower(concat('%', :shift, '%')))")
+    List<Course> findCoursesByFilter(
+            @Param("name") String name,
+            @Param("shift") String shift
+    );
     default List<Course> findCourses() {
         return findAll();
     }
-    Optional<Course> findByCode(String code);
 }
